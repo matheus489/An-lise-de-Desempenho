efa3211dd572b6fac1c97a1939d748eea7b2a75e
matheus489 <matheus.rubem@ufrpe.br>
2024-04-29 23:43:25 -0300

    implementação em java e inicio de implementação em c

---
+++ Analise/Main.java
@@ -0,0 +1,111 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        executeActions("D:/Faculdade/Analise/arq.txt");
+    }
+
+    static void executeActions(String fileName) {
+        try {
+            File file = new File(fileName);
+            Scanner scanner = new Scanner(file);
+            String[] data = scanner.nextLine().strip().split(" ");
+            LinkedList linkedList = new LinkedList();
+            for (String item : data) {
+                linkedList.append(Integer.parseInt(item));
+            }
+
+            int actionCount = Integer.parseInt(scanner.nextLine().strip());
+
+            for (int i = 0; i < actionCount; i++) {
+                String[] action = scanner.nextLine().strip().split(" ");
+                if (action[0].equals("A")) {
+                    int number = Integer.parseInt(action[1]);
+                    int position = Integer.parseInt(action[2]);
+                    if (position == 0) {
+                        Node newNode = new Node(number);
+                        newNode.next = linkedList.head;
+                        linkedList.head = newNode;
+                    } else {
+                        Node node = linkedList.head;
+                        for (int j = 0; node != null && j < position - 1; j++) {
+                            node = node.next;
+                        }
+                        if (node == null)
+                            return;
+                        Node newNode = new Node(number);
+                        newNode.next = node.next;
+                        node.next = newNode;
+                    }
+                } else if (action[0].equals("R")) {
+                    int position = Integer.parseInt(action[1]);
+                    linkedList.remove(position);
+                } else if (action[0].equals("P")) {
+                    linkedList.printList();
+                }
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    Node head;
+
+    LinkedList() {
+        this.head = null;
+    }
+
+    void append(int data) {
+        Node new_node = new Node(data);
+        if (this.head == null) {
+            this.head = new_node;
+            return;
+        }
+        Node last_node = this.head;
+        while (last_node.next != null) {
+            last_node = last_node.next;
+        }
+        last_node.next = new_node;
+    }
+
+    void remove(int position) {
+        if (this.head == null)
+            return;
+        if (position == 0) {
+            this.head = this.head.next;
+            return;
+        }
+        Node node = this.head;
+        for (int i = 0; node != null && i < position - 1; i++) {
+            node = node.next;
+        }
+        if (node == null || node.next == null)
+            return;
+        node.next = node.next.next;
+    }
+
+    void printList() {
+        Node node = this.head;
+        while (node != null) {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+}
+
---
+++ Analise/list.py
@@ -0,0 +1,77 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+
+    def remove(self, position):
+        if not self.head:
+            return
+        if position == 0:
+            self.head = self.head.next
+            return
+        node = self.head
+        for _ in range(position - 1):
+            if node.next:
+                node = node.next
+            else:
+                return
+        if node.next:
+            node.next = node.next.next
+
+    def print_list(self):
+        node = self.head
+        while node:
+            print(node.data, end=' ')
+            node = node.next
+        print()
+
+# Função para executar as ações conforme o arquivo
+def execute_actions(file_name):
+    with open(file_name, 'r') as file:
+        data = file.readline().strip().split()
+        linked_list = LinkedList()
+        for item in data:
+            linked_list.append(int(item))
+
+        Ação = int(file.readline().strip())
+
+        for _ in range(Ação):
+            Ação = file.readline().strip().split()
+            if Ação[0] == 'A':
+                number = int(Ação[1])
+                Posição = int(Ação[2])
+                if Posição == 0:
+                    linked_list.head = Node(number)
+                    linked_list.head.next = linked_list.head
+                else:
+                    node = linked_list.head
+                    for _ in range(Posição - 1):
+                        if node.next:
+                            node = node.next
+                        else:
+                            return
+                    new_node = Node(number)
+                    new_node.next = node.next
+                    node.next = new_node
+            elif Ação[0] == 'R':
+                Posição = int(Ação[1])
+                linked_list.remove(Posição)
+            elif Ação[0] == 'P':
+                linked_list.print_list()
+
+# Executando as ações conforme o arquivo arq.txt
+execute_actions("D:/Faculdade/Analise/arq.txt")
